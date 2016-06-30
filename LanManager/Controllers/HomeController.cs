cf93e255738a83bff14cabbeba927f6814b12304
Oscar Vicente Perez <wellan_heavy@hotmail.com>
2016-06-30 23:57:36 +0200

    Initial commit

---
+++ LanManager/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using LanManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new Equipo() { Name = "Oscar-Pc" });
+        }
+
+        [HttpPost]
+        public ActionResult Index(Equipo equipo)
+        {
+            return View(equipo);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LanManager/Models/Equipo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanManager.Models
+{
+    public class Equipo
+    {
+        public String Name { get; set; }
+        public String IpAddress { get; set; }
+        public String MacAddress { get; set; }
+
+        public Equipo()
+        {
+            Name = String.Empty;
+            IpAddress = String.Empty;
+            MacAddress = String.Empty;
+        }
+    }
+}
